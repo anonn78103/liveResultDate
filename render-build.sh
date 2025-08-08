7620120f2c96ca47378a9c1f5c6764a72498bec6
anonn78103 <139428061+anonn78103@users.noreply.github.com>
2025-08-08 19:56:06 +0530

    Update and rename render-build.sh to start.sh

--- render-build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Install latest Chrome for Linux
-apt-get update && apt-get install -y wget gnupg unzip curl
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -y ./google-chrome-stable_current_amd64.deb || apt --fix-broken install -y
---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install Chrome
+apt-get update
+apt-get install -y wget gnupg2
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+apt-get update
+apt-get install -y google-chrome-stable
+
+# Launch your app
+python app.py
